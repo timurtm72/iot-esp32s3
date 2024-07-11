6d0bf56819c4dc4134a201e1d268fe6d8db15479
timurtm72 <timur.sultanov1972@gmail.com>
2024-07-11 19:35:23 +0300

    reformat update methode

--- src/main/java/dev/timur/example/iotesp32s3/serviceimpl/DeviceServiceImpl.java
+++ src/main/java/dev/timur/example/iotesp32s3/serviceimpl/DeviceServiceImpl.java
@@ -3,7 +3,9 @@ package dev.timur.example.iotesp32s3.serviceimpl;
 import dev.timur.example.iotesp32s3.dto.DeviceDto;
 import dev.timur.example.iotesp32s3.enums.Status;
 import dev.timur.example.iotesp32s3.mapper.DeviceMapper;
+import dev.timur.example.iotesp32s3.model.BitDeviceData;
 import dev.timur.example.iotesp32s3.model.Device;
+import dev.timur.example.iotesp32s3.model.StripLedData;
 import dev.timur.example.iotesp32s3.repository.DeviceRepository;
 import dev.timur.example.iotesp32s3.service.DeviseService;
 import dev.timur.example.iotesp32s3.utils.MapperUtil;
@@ -63,22 +65,54 @@ public class DeviceServiceImpl implements DeviseService {
     @Transactional
     @Override
     public Status update(DeviceDto deviceDto, Long id) {
-        if(deviceDto == null){
+        if (deviceDto == null) {
             return Status.IS_EMPTY;
         }
         deviceDto.setId(id);
         Device device = deviceMapper.toEntity(deviceDto);
         Device updateDevice = deviceRepository.findDevicesById(id).orElse(null);
-        if(updateDevice == null){
+        if (updateDevice == null) {
             return Status.IS_NOT_FOUND;
         }
-        updateDevice.setName(device.getName());
-        updateDevice.setDescription(device.getDescription());
-        updateDevice.setInputValues(device.getInputValues());
-        updateDevice.setLedValues(device.getLedValues());
+
+        // Update name if it is not empty or null
+        if (device.getName() != null && !device.getName().isEmpty()) {
+            updateDevice.setName(device.getName());
+        }
+
+        // Update description if it is not empty or null
+        if (device.getDescription() != null && !device.getDescription().isEmpty()) {
+            updateDevice.setDescription(device.getDescription());
+        }
+
+        // Update inputValues
+        List<BitDeviceData> existingInputValues = updateDevice.getInputValues();
+        List<BitDeviceData> editingInputValues = device.getInputValues();
+
+        int sizeEx = existingInputValues.size() - 1;
+        int sizeEd = editingInputValues.size() - 1;
+
+        if(existingInputValues.get(sizeEx) != device.getInputValues().get(sizeEd)){
+            existingInputValues.add(editingInputValues.get(sizeEd));
+            updateDevice.setInputValues(existingInputValues);
+        }
+
+        // Update ledValues
+        List<StripLedData> existingLedValues = updateDevice.getLedValues();
+        List<StripLedData> editingLedValues = device.getLedValues();
+
+        sizeEx = existingLedValues.size() - 1;
+        sizeEd = editingLedValues.size() - 1;
+
+        if(existingLedValues.get(sizeEx) != device.getLedValues().get(sizeEd)){
+            existingLedValues.add(editingLedValues.get(sizeEd));
+            updateDevice.setLedValues(existingLedValues);
+        }
+
         deviceRepository.save(updateDevice);
         return Status.IS_OK;
     }
+
     @Transactional
     @Override
     public Status delete(Long id) {
